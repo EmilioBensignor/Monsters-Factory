074e4ccf85fcf048cd87e369a4ec6542e844a7d0
Lio <liobensignor@gmail.com>
2023-11-07 18:36:57 -0300

    Arrancando

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      light
-      color="primary"
-    >
-      <div class="d-flex justify-center">
-        <ul class="d-flex flex-row justify-center">
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/monstruos">Mis Monstruos</router-link></li>
-          <li><router-link to="/museo">Museo</router-link></li>
-        </ul>
-      </div>
-    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app>
 
+      <v-list tag="ul">
+        <li v-for="item in menu" :key="item.titulo">
+          <v-list-item :to="item.path">
+            <v-list-item-action>
+              <v-icon color="primary">mdi-{{ item.icono }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title> {{ item.titulo }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </li>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar class="primary" app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Monster Factory</v-toolbar-title>
+    </v-app-bar>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
@@ -26,11 +32,26 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    menu: [
+      {
+        titulo: 'Home',
+        icono: 'home',
+        path: '/'
+      },
+      {
+        titulo: 'Mis Monstruos',
+        icono: 'folder',
+        path: '/monsters'
+      },
+      {
+        titulo: 'Museo',
+        icono: 'widgets',
+        path: '/museo'
+      }
+    ],
+    drawer: null,
   }),
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
